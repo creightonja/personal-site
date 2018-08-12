d13ca7b52451fe1e5233c9337ac016f58bdd8d65
Jason Creighton <jason.creighton@chromaplex.io>
2018-08-12 17:06:17 +0900

    git rid of laravel vue boilerplate, add in csrf token to meta, add javascript to make mobile nav function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <title>@yield('title')</title>
+        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         @yield('stylesheets')
@@ -11,11 +14,14 @@
     <body>
         @include('partials.header')
         <section class="section">
-            <div class="container">
+            <div id="app" class="container">
                 @yield('content')
             </div>
         </section>
 
         @include('partials.footer')
+
+        
+        <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
     </body>
 </html>
