1e65bb1e6cb67d8ddb8aae89c0e19a07738849df
Jason Creighton <jason.creighton@chromaplex.io>
2018-08-14 23:29:36 +0900

    add google analytics to main app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,5 +26,15 @@
         </footer>
         
         <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123889166-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-123889166-1');
+        </script>
+
     </body>
 </html>
